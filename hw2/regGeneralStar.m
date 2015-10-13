f6e4d31a93a03cd0f91ba8d25eb98c4e037ad2e9
andyyuan <iamyuanchung@gmail.com>
2015-10-14 00:31:08 +0800

    finish Q11a

---
+++ hw2/regGeneralStar.m
@@ -0,0 +1,9 @@
+function ans = regGeneralStar(n, k)
+
+% plot the circle centered at (0, 0) and whose radius equals to 1
+theta = linspace(0, 2 * pi);
+plot(exp(j * theta), 'color', 'r'); hold on;
+
+% plot the polygon
+phi = 0: (2 * pi / n * k): (k * 2 * pi);
+plot(exp(j * phi), 'color', 'b');
