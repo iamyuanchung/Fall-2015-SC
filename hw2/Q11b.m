2c9e1066e9964fd086a20ad88c7b20856819e4a3
andyyuan <iamyuanchung@gmail.com>
2015-10-20 13:25:37 +0800

    demo done

--- hw2/Q11b.m
+++ hw2/Q11b.m
@@ -1,14 +1,14 @@
-% figure(1);
-% filename = 'Q11b.gif';
-for i = 1:1000
+figure(1);
+filename = 'Q11b.gif';
+for i = 1:200
     regGeneralStar(79, i);
     drawnow;
-    % frame = getframe(1);
-    % im = frame2im(frame);
-    % [imind, cm] = rgb2ind(im, 256);
-    % if i == 1;
-    %     imwrite(imind, cm, filename, 'gif', 'Loopcount', inf);
-    % else
-    %     imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append');
-    % end
+    frame = getframe(1);
+    im = frame2im(frame);
+    [imind, cm] = rgb2ind(im, 256);
+    if i == 1;
+        imwrite(imind, cm, filename, 'gif', 'Loopcount', inf);
+    else
+        imwrite(imind, cm, filename, 'gif', 'WriteMode', 'append');
+    end
 end
